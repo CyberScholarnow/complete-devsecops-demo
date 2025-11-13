b8301e79d4a87d557a9601c07a593ec0b666971d
CyberScholarnow <131221708+CyberScholarnow@users.noreply.github.com>
2025-11-13 15:39:35 -0500

    Create main.tf
    toy Terraform to scan; you won’t apply it

---
+++ iac/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 1.5.0"
+}
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_s3_bucket" "example" {
+  bucket = "example-nonprod-bucket-name-change-me"
+  acl    = "private"
+  # Intentionally using versioning disabled for demo (tools should flag)
+  versioning {
+    enabled = false
+  }
+}
